97a0873a2991d08d9f7451f83f0e325f09bab46c
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2023-12-06 23:30:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
   // your code here
+	if(!text || text.trim() === ' ');
+	return '';
 }
 
 // Do not change the code below
+for(let char of text){
+	if(char !==' ');
+	return char;
+}
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
